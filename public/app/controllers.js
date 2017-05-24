57df9cf08be0aaf8455bde66c69bf4d3adccef03
stevenkeiser <stevekeiser@gmail.com>
2017-05-24 12:42:16 -0700

    minor cleanup

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -25,6 +25,14 @@ angular.module('TaskCtrls', ['TaskServices'])
         }, function error(data) {
             console.log(data);
         });
+        $scope.deleteTask = function(id, tasksIdx) {
+            Task.delete({ id: id }, function success(data) {
+                $scope.tasks.splice(tasksIdx, 1);
+            }, function error(data) {
+                console.log(data);
+            });
+        };
+
     }])
     .controller('ShowAllCtrl', ['$scope', 'Task', function($scope, Task) {
         $scope.tasks = [];
@@ -51,7 +59,7 @@ angular.module('TaskCtrls', ['TaskServices'])
 
         $scope.createTask = function() {
             Task.save($scope.task, function success(data) {
-                $location.path('/');
+                $location.path('/tasks/');
             }, function error(data) {
                 console.log(data);
             });
@@ -173,7 +181,6 @@ angular.module('TaskCtrls', ['TaskServices'])
 
 
 
-
     // tasks.forEach(function(task) {
     //     ctx = document.getElementById("myChart" + task.id).getContext('2d');
     //     var myDougnutChart = new Chart(ctx, {
@@ -191,4 +198,5 @@ angular.module('TaskCtrls', ['TaskServices'])
     //         }
     //     });
     // });
+
 }]);
