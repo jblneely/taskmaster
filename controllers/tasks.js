e3f92b3a1acb6a5932cdd8f03761c3b1ef7ff210
jblneely <bowman.neely@gmail.com>
2017-05-25 15:40:31 -0700

    routed data into tasks and users

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var Task = require('../models/task');
+var User = require('../models/user');
 var router = express.Router();
 
 router.route('/')
@@ -7,12 +8,33 @@ router.route('/')
         Task.find(function(err, tasks) {
             if (err) return res.status(500).send(err);
             return res.send(tasks);
-        });
+        }).populate('user');
+
     })
     .post(function(req, res) {
         Task.create(req.body, function(err, task) {
             if (err) return res.status(500).send(err);
             return res.send(task);
+        });
+    })
+    .put(function(req, res) {
+        Task.findByIdAndUpdate(req.body.id, {
+            completed: true,
+            completedDate: Date.now()
+        }, function(err, task) {
+            if (err) return res.status(500).send(err);
+            //User table add taskId to completed
+
+            console.log('user is is', req.body.userId);
+            User.findByIdAndUpdate(req.body.userId, { $push: { completedTask: req.body.id } },
+                function(err, user) {
+                    if (err) {
+                        console.log('user not updated', err);
+                    } else {
+                        console.log('user was updated', user);
+                    }
+                    return res.send({ task: task });
+                });
         });
     });
 
@@ -21,12 +43,6 @@ router.route('/:id')
         Task.findById(req.params.id, function(err, task) {
             if (err) return res.status(500).send(err);
             return res.send(task);
-        });
-    })
-    .put(function(req, res) {
-        Task.findByIdAndUpdate(req.params.id, req.body, function(err) {
-            if (err) return res.status(500).send(err);
-            return res.send({ message: 'success' });
         });
     })
     .delete(function(req, res) {
